cc278b9547dbe4409a6b23a977791e6f95ae013c
RitijThakur <144477554+RitijThakur@users.noreply.github.com>
2023-09-09 16:46:51 +0000

    container confige done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,5 +2,15 @@ const AccountId=14444
 let EmailId="ritij@gmail.com"
 var AccountPassword="45454"
 accountcity="jaipur"
+let accountState;
+/*
+prefer not to use var 
+because of problem in block scope and functional scope 
+*/
 
-console.table([AccountId,EmailId,AccountPassword])
+console.table([AccountId,EmailId,AccountPassword,accountState])
+
+/*
+you can see that we have defined a variable using let i.e accountState,but we have not given any value to it 
+so when we run it js gives us message as undefined so if not given value to variable js considers it as undefined.
+*/
