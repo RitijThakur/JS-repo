bcf26faa6be3f9892ba4c828fb2dffb169a02a4b
RitijThakur <144477554+RitijThakur@users.noreply.github.com>
2023-09-21 15:06:11 +0000

    datatypes

---
+++ 01_Basics/02_datattypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all Js code as new version
+// alert(3+3) // we are using node js not browser, we can use alert in node also but it has different syntax
+
+let name="Ritij";
+let age=20;
+let isLoggedIn=true;
+
+/* primitive data types 
+   numbers --> 2^53
+   bigint -->  
+   string --> "  " collection of words r
+   boolean --> true/false
+   null --> standalone value
+   undefined --> no value has been assigned yet
+   symbols --> unique 
+*/
+  // object 
+  console.log(undefined);//undefined
+  console.log(null); //object
+  
+
+
+
+
+
+
+
+
